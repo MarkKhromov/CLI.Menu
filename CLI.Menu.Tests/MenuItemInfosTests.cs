c0cd247e141797b6389c6e5df3ba2d230a321033
Mark Khromov <lane.71rus@gmail.com>
2017-12-06 22:11:34 +0300

    Added menu item infos tests

---
+++ CLI.Menu.Tests/MenuItemInfosTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CLI.Menu.Tests {
+    [TestFixture]
+    public class MenuItemInfosTests {
+        [Test]
+        public void AddTest() {
+            var menuItemInfos = new MenuItemInfos();
+            Assert.IsTrue(menuItemInfos.Add(new MenuItemInfo(null, null)));
+            Assert.IsTrue(menuItemInfos.Add(new MenuItemInfo(null, null)));
+            Assert.IsTrue(menuItemInfos.Add(new MenuItemInfo(null, null)));
+            Assert.IsTrue(menuItemInfos.Add(new MenuItemInfo(null, null)));
+            Assert.IsTrue(menuItemInfos.Add(new MenuItemInfo(null, null)));
+            Assert.IsTrue(menuItemInfos.Add(new MenuItemInfo(null, null)));
+            Assert.IsTrue(menuItemInfos.Add(new MenuItemInfo(null, null)));
+            Assert.IsTrue(menuItemInfos.Add(new MenuItemInfo(null, null)));
+            Assert.IsTrue(menuItemInfos.Add(new MenuItemInfo(null, null)));
+            Assert.IsFalse(menuItemInfos.Add(new MenuItemInfo(null, null)));
+        }
+
+        [Test]
+        public void AccessByConsoleKeyTest() {
+            var menuItemInfos = new MenuItemInfos();
+            var menuItem1 = new MenuItemInfo(null, null);
+            var menuItem2 = new MenuItemInfo(null, null);
+            Assert.AreNotEqual(menuItem1, menuItem2);
+            Assert.IsTrue(menuItemInfos.Add(menuItem1));
+            Assert.IsTrue(menuItemInfos.Add(menuItem2));
+            Assert.AreEqual(menuItem1, menuItemInfos[ConsoleKey.D1]);
+            Assert.AreEqual(menuItem2, menuItemInfos[ConsoleKey.D2]);
+            Assert.AreEqual(null, menuItemInfos[ConsoleKey.D3]);
+        }
+    }
+}
